725e171e6d7564d010f84382aaaef3d564c56dc1
RushIsReal <51927770+RushIsReal@users.noreply.github.com>
2024-11-09 08:01:55 -0300

    https://leetcode.com/problems/longest-substring-without-repeating-characters solution

---
+++ longest-substring-without-repeating-characters/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+LengthOfLongestSubstring("dvdf");
+
+static void LengthOfLongestSubstring(string s)
+{
+    string looped = "";
+    int result = 0;
+    for (var i = 0; s.Length > i; i++)
+    {
+        for (var j = i; s.Length > j; j++)
+        {
+            if (looped.Contains(s[j]))
+            {
+                result = (result < looped.Length) ? looped.Length : result;
+                looped = "";
+                break;
+            }
+            looped += s[j];
+        }
+    }
+    result = (result < looped.Length) ? looped.Length : result;
+    Console.WriteLine(result);
+}
